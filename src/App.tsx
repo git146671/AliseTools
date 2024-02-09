546c15cc0e01da16d0ec2bd87e7f24ba090e8fa1
Anton Plekhanov <146661@mail.ru>
2024-02-09 23:46:39 +0300

    стили и фиксы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import {TimeHelper} from "./components/TimeHelper";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
+    <MainContainer>
       <TimeHelper/>
-    </div>
+    </MainContainer>
   );
 }
 
+const MainContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #3b3b3b;
+    `
 export default App;
--- src/components/TimeHelper.tsx
+++ src/components/TimeHelper.tsx
@@ -22,53 +22,79 @@ export const TimeHelper = () => {
 
     function getMinutes(destinationTime: string) {
         var c = destinationTime.split(':');
-        console.log(c[0]);
         if (c[0] === "") c[0] = "0";
         if (c[1] === "") c[1] = "0";
         return parseInt(c[0]) * 60 + parseInt(c[1]);
     }
 
     function processTimeHandler() {
-        var destMinutes = getMinutes(destinationTime);
-        var trMinutes = getMinutes(travelTime);
-        var resMinutes = getMinutes(reserveTime);
-        var result = destMinutes - trMinutes - resMinutes;
-        var resHours = Math.floor(result/60) === 0 ? '00' : '' + Math.floor(result/60);
-        var resMins = result % 60 === 0 ? "00" : result % 60;
-        setStartTime(+ resHours + ":" + resMins);
+        if (destinationTime === "" || travelTime === "" || reserveTime === "") {
+            setStartTime("заполни все пустые поля!")
+        } else {
+            var destMinutes = getMinutes(destinationTime);
+            var trMinutes = getMinutes(travelTime);
+            var resMinutes = getMinutes(reserveTime);
+            var result = destMinutes - trMinutes - resMinutes;
+            var resHours = Math.floor(result / 60) === 0 ? '00' : '' + Math.floor(result / 60);
+            var resMins = result % 60 === 0 ? "00" : "" + result % 60;
+            if (resMins.length === 1) {
+                resMins = "0" + resMins;
+            }
+            setStartTime('' + resHours + ":" + resMins);
+        }
     }
 
     return (
         <TimeHelperContainer>
-            <Header>Привет! Я помогу тебе приехать вовремя!</Header>
-            <span>  1. Введи, во сколько тебе надо быть на месте:  </span>
-            <input type={"time"} onChange={setDestinationTimeHandler} value={destinationTime}/>
+            <Header>Привет. Я помогу тебе приехать вовремя.</Header>
+            <span>  1. Во сколько тебе надо быть на месте:  </span>
+            <StyledInput type={"time"} onChange={setDestinationTimeHandler} value={destinationTime}/>
             <br/>
-            <span>  2. Введи, сколько тебе добираться до места:  </span>
-            <input type={"time"} onChange={setTravelTimeHandler} value={travelTime}/>
+            <span>  2. Сколько тебе добираться до места:  </span>
+            <StyledInput type={"time"} onChange={setTravelTimeHandler} value={travelTime}/>
             <br/>
-            <span>  3. Введи, насколько заранее ты хочешь приехать:  </span>
-            <input type={"time"} onChange={setReserveTimeHandler} value={reserveTime}/>
+            <span>  3. Насколько заранее ты хочешь приехать:  </span>
+            <StyledInput type={"time"} onChange={setReserveTimeHandler} value={reserveTime}/>
             <br/>
-            <button onClick={processTimeHandler}>Посчитать!</button>
+            <StyledBtn onClick={processTimeHandler}>Посчитать</StyledBtn>
             <br/>
-            {startTime && <span>Тебе надо выйти в {startTime}</span>}
+            {startTime && <span>Тебе надо выйти в: {startTime}</span>}
         </TimeHelperContainer>
     )
 }
 
 const TimeHelperContainer = styled.div`
     position: fixed;
-    top: 10%;
+    top: 5%;
     left: 50%;
-    width:15em;
-    height:25em;
-    margin-left: -8em; /*set to a negative number 1/2 of your width*/
-    border: 1px solid #ccc;
-    background-color: #f3f3f3;
+    width:300px;
+    height:400px;
+    margin-left: -155px; /*set to a negative number 1/2 of your width*/
+    border: 2px solid #33fff2;
+    background-color: #3b3b3b;
+    color: #33fff2;
+    padding: 17px;  
+    font-size: 20px;
     `
 
 const Header = styled.h3`
-    padding: 7px;
-    color: #676767;
+    padding: 0 7px;
+    color: #33fff2;
+    
+`
+
+const StyledInput = styled.input`
+    background-color: #33fff2;
+    width: 80px;
+    height: 25px;
+`
+
+const StyledBtn = styled.button`
+    background-color: #33fff2;
+    width: 110px;
+    height: 40px;
+    color: #3b3b3b;
+    font-size: 18px;
+    border-radius: 4px;
+    margin: 10px 0 5px 95px;
 `
